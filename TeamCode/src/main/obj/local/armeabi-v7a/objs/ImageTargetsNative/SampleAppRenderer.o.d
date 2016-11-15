794e8c190bb0b48c6fa0980fbe98b2bfda8b3f8f
BuildingAtom <84liadam@gmail.com>
2016-11-14 21:20:57 -0500

    Setup native code and imported an example (that is nonworking because it is imported). It may or may not be 100% working, but it should be enough to start with. You need to install Android NDK @http://developer.android.com/ndk/index.html Extract the zip to the same folder you want to install the NDK in and set the path to the NDK in the project structure under File.

---
+++ TeamCode/src/main/obj/local/armeabi-v7a/objs/ImageTargetsNative/SampleAppRenderer.o.d
@@ -0,0 +1,92 @@
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/obj/local/armeabi-v7a/objs/ImageTargetsNative/SampleAppRenderer.o: \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/SampleAppRenderer.cpp \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/SampleAppRenderer.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Vuforia.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/System.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/CameraDevice.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/NonCopyable.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/VideoMode.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/CameraCalibration.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Vectors.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/CameraField.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Renderer.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Matrices.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/State.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Frame.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/VideoBackgroundConfig.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Tool.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Device.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Type.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/ViewerParameters.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/ViewerParametersList.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/RenderingPrimitives.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/View.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/ViewList.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Mesh.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/GLRenderer.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/StateUpdater.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/TrackerManager.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Tracker.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/CubeShaders.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/ImageTargets.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/SampleUtils.h
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/SampleAppRenderer.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Vuforia.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/System.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/CameraDevice.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/NonCopyable.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/VideoMode.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/CameraCalibration.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Vectors.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/CameraField.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Renderer.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Matrices.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/State.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Frame.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/VideoBackgroundConfig.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Tool.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Device.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Type.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/ViewerParameters.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/ViewerParametersList.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/RenderingPrimitives.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/View.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/ViewList.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Mesh.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/GLRenderer.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/StateUpdater.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/TrackerManager.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/../jni_src/include\Vuforia/Tracker.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/CubeShaders.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/ImageTargets.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/SampleUtils.h:
---
+++ TeamCode/src/main/obj/local/armeabi-v7a/objs/ImageTargetsNative/Texture.o.d
@@ -0,0 +1,8 @@
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/obj/local/armeabi-v7a/objs/ImageTargetsNative/Texture.o: \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/Texture.cpp \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/Texture.h \
+  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/SampleUtils.h
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/Texture.h:
+
+D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/SampleUtils.h:
