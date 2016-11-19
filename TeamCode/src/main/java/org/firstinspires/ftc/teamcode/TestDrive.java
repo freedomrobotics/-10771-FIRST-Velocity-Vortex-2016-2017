22a9f98ff2481ac7ed300a1af94440f37f0d6146
freedomroboticsm <freedomrobotics@gmail.com>
2016-11-19 12:58:27 -0500

    fixed most bugs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
@@ -23,10 +23,10 @@ public class TestDrive extends LinearOpMode{
         motorA = hardwareMap.dcMotor.get("motorFrontRight");
         motorB = hardwareMap.dcMotor.get("motorFrontLeft");
         motorC = hardwareMap.dcMotor.get("motorBackLeft");
-        motorD = hardwareMap.dcMotor.get("motorBackRight");
+        motorD = hardwareMap.dcMotor.get("motorBackRight"); //initializes motor
 
-        motorB.setDirection(DcMotor.Direction.REVERSE);
-        motorC.setDirection(DcMotor.Direction.REVERSE);
+        motorB.setDirection(DcMotor.Direction.REVERSE); //reverses motor b to make positive rotation equal to positive translational motion
+        motorC.setDirection(DcMotor.Direction.REVERSE); //reverses motor c to make positive rotation equal to positive translational motion
 
         waitForStart();
 
@@ -35,26 +35,27 @@ public class TestDrive extends LinearOpMode{
             //double leftYAxis = -gamepad1.left_stick_y;
             //double leftXAxis = gamepad1.left_stick_x;
 
-            double joystickTheta = Math.atan2((-gamepad1.left_stick_y),(gamepad1.left_stick_x));
-            double joystickRadius = Math.sqrt((gamepad1.left_stick_x)*(gamepad1.left_stick_x)+(-gamepad1.left_stick_y)*(-gamepad1.left_stick_y));
 
-            if((gamepad1.left_stick_x==0)&&(gamepad1.left_stick_y==0)){
+            double joystickTheta = Math.atan2((-gamepad1.left_stick_y),(gamepad1.left_stick_x)); //declares the angle of joystick position in standard polar coordinates
+            double joystickRadius = Math.sqrt((gamepad1.left_stick_x)*(gamepad1.left_stick_x)+(gamepad1.left_stick_y)*(gamepad1.left_stick_y)); //declares the magnitude of the radius of the joystick position
+
+            if((gamepad1.left_stick_x==0)&&(gamepad1.left_stick_y==0)) /**/{
                 if (gamepad1.left_trigger != 0) {
-                    motorA.setPower(gamepad1.left_trigger);
-                    motorB.setPower(-(gamepad1.left_trigger));
-                    motorC.setPower(-(gamepad1.left_trigger));
-                    motorD.setPower(gamepad1.left_trigger);
+                    motorA.setPower(-(gamepad1.right_trigger)); //sets motor a to spin the robot CW to the magnitude of the left trigger
+                    motorB.setPower(gamepad1.right_trigger); //sets motor a to spin the robot CW to the magnitude of the left trigger
+                    motorC.setPower(gamepad1.right_trigger); //sets motor a to spin the robot CW to the magnitude of the left trigger
+                    motorD.setPower(-(gamepad1.right_trigger)); //sets motor a to spin the robot CW to the magnitude of the left trigger
                 } else {
-                    motorA.setPower(-(gamepad1.right_trigger));
-                    motorB.setPower(gamepad1.right_trigger);
-                    motorC.setPower(gamepad1.right_trigger);
-                    motorD.setPower(-(gamepad1.right_trigger));
+                    motorA.setPower(gamepad1.left_trigger); //sets motor a to spin the robot CCW to the magnitude of the left trigger
+                    motorB.setPower(-(gamepad1.left_trigger)); //sets motor a to spin the robot CCW to the magnitude of the left trigger
+                    motorC.setPower(-(gamepad1.left_trigger)); //sets motor a to spin the robot CCW to the magnitude of the left trigger
+                    motorD.setPower(gamepad1.left_trigger); //sets motor a to spin the robot CCW to the magnitude of the left trigger
                 }
             }else{
-                motorA.setPower((Math.sin(Math.sin(joystickTheta-(Math.PI/2))))*joystickRadius);
-                motorB.setPower((Math.cos(Math.sin(joystickTheta-(Math.PI/2))))*joystickRadius);
-                motorC.setPower((Math.sin(Math.sin(joystickTheta-(Math.PI/2))))*joystickRadius);
-                motorD.setPower((Math.cos(Math.sin(joystickTheta-(Math.PI/2))))*joystickRadius);
+                motorA.setPower((Math.sin(joystickTheta-(Math.PI/4)))*joystickRadius);
+                motorB.setPower((Math.cos(joystickTheta-(Math.PI/4)))*joystickRadius);
+                motorC.setPower((Math.sin(joystickTheta-(Math.PI/4)))*joystickRadius);
+                motorD.setPower((Math.cos(joystickTheta-(Math.PI/4)))*joystickRadius);
 
                 /*
                 motorA.setPower((-leftXAxis+leftYAxis)/2);
