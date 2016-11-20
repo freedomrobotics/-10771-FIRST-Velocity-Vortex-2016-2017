abab1eba18e2dfe1383161f251ae79ee719f6e53
Matthew <matthew.r.sabatino@gmail.com>
2016-11-20 14:34:55 -0500

    added in code comments for TestDrive op mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
@@ -12,6 +12,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name = "TestDrive")
 public class TestDrive extends LinearOpMode{
 
+    //initializes motors in order of standard graph quadrants
     private DcMotor motorA;
     private DcMotor motorB;
     private DcMotor motorC;
@@ -20,52 +21,50 @@ public class TestDrive extends LinearOpMode{
     @Override
     public void runOpMode() throws InterruptedException{
 
-        motorA = hardwareMap.dcMotor.get("motorFrontRight");
-        motorB = hardwareMap.dcMotor.get("motorFrontLeft");
-        motorC = hardwareMap.dcMotor.get("motorBackLeft");
-        motorD = hardwareMap.dcMotor.get("motorBackRight"); //initializes motor
+        motorA = hardwareMap.dcMotor.get("motorFrontRight"); //sets variable motorA to front right motor
+        motorB = hardwareMap.dcMotor.get("motorFrontLeft"); //sets variable motorB to front left motor
+        motorC = hardwareMap.dcMotor.get("motorBackLeft"); //sets variable motorC to back left motor
+        motorD = hardwareMap.dcMotor.get("motorBackRight"); //sets variable motorD to back right motor
 
-        motorB.setDirection(DcMotor.Direction.REVERSE); //reverses motor b to make positive rotation equal to positive translational motion
-        motorC.setDirection(DcMotor.Direction.REVERSE); //reverses motor c to make positive rotation equal to positive translational motion
+        //reverses left side motors to make forwards positive rotation for all motors.
+        motorB.setDirection(DcMotor.Direction.REVERSE);
+        motorC.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
-
         while(opModeIsActive()){
-
-            //double leftYAxis = -gamepad1.left_stick_y;
-            //double leftXAxis = gamepad1.left_stick_x;
-
 
             double joystickTheta = Math.atan2((gamepad1.left_stick_y),(gamepad1.left_stick_x)); //declares the angle of joystick position in standard polar coordinates
             double joystickRadius = Math.sqrt((gamepad1.left_stick_x)*(gamepad1.left_stick_x)+(gamepad1.left_stick_y)*(gamepad1.left_stick_y)); //declares the magnitude of the radius of the joystick position
 
-            if((gamepad1.left_stick_x==0)&&(gamepad1.left_stick_y==0)) /**/{
-                if (gamepad1.left_trigger != 0) {
-                    motorA.setPower(-(gamepad1.right_trigger)); //sets motor a to spin the robot CW to the magnitude of the left trigger
-                    motorB.setPower(gamepad1.right_trigger); //sets motor a to spin the robot CW to the magnitude of the left trigger
-                    motorC.setPower(gamepad1.right_trigger); //sets motor a to spin the robot CW to the magnitude of the left trigger
-                    motorD.setPower(-(gamepad1.right_trigger)); //sets motor a to spin the robot CW to the magnitude of the left trigger
-                } else {
-                    motorA.setPower(gamepad1.left_trigger); //sets motor a to spin the robot CCW to the magnitude of the left trigger
-                    motorB.setPower(-(gamepad1.left_trigger)); //sets motor a to spin the robot CCW to the magnitude of the left trigger
-                    motorC.setPower(-(gamepad1.left_trigger)); //sets motor a to spin the robot CCW to the magnitude of the left trigger
-                    motorD.setPower(gamepad1.left_trigger); //sets motor a to spin the robot CCW to the magnitude of the left trigger
+            if((gamepad1.left_stick_x==0)&&(gamepad1.left_stick_y==0))/*if either triggers are pressed*/{
+
+                if (gamepad1.left_trigger != 0)/*if the left trigger is pressed*/{
+                    //CCW zero clarence rotation (speed determined my magnitude of left trigger)
+                    motorA.setPower(-(gamepad1.right_trigger));
+                    motorB.setPower(gamepad1.right_trigger);
+                    motorC.setPower(gamepad1.right_trigger);
+                    motorD.setPower(-(gamepad1.right_trigger));
+                }else/*if only the right trigger is pressed*/{
+                    //CW zero clarence rotation (speed determined my magnitude of right trigger)
+                    motorA.setPower(gamepad1.left_trigger);
+                    motorB.setPower(-(gamepad1.left_trigger));
+                    motorC.setPower(-(gamepad1.left_trigger));
+                    motorD.setPower(gamepad1.left_trigger);
                 }
-            }else{
-                motorA.setPower((Math.sin(joystickTheta-(Math.PI/4)))*joystickRadius);
-                motorB.setPower((Math.cos(joystickTheta-(Math.PI/4)))*joystickRadius);
+
+            }else/*if neither triggers are pressed*/{
+
+                //translational movement determined by vector of left joystick
+                motorA.setPower((Math.sin(joystickTheta-(Math.PI/4)))*joystickRadius); //same power as motor C but in opposite direction
+                motorB.setPower((Math.cos(joystickTheta-(Math.PI/4)))*joystickRadius); //same power as motor D but in opposite direction
                 motorC.setPower((Math.sin(joystickTheta-(Math.PI/4)))*joystickRadius);
                 motorD.setPower((Math.cos(joystickTheta-(Math.PI/4)))*joystickRadius);
 
-                /*
-                motorA.setPower((-leftXAxis+leftYAxis)/2);
-                motorD.setPower((leftXAxis+leftYAxis)/2);
-                motorC.setPower((-leftXAxis+leftYAxis)/2);
-                motorD.setPower((leftXAxis+leftYAxis)/2);
-                */
+
             }
 
             idle();
+
         }
     }
 }
