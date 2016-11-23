ecdb03a0cd0f180ba8ee9b4b230ca80f4bdde712
freedomroboticsm <freedomrobotics@gmail.com>
2016-11-23 11:40:33 -0500

    Deleted Old Commented Sections

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
@@ -28,33 +28,7 @@ public class TestDrive extends LinearOpMode{
 
         waitForStart();
         while(opModeIsActive()){
-
-            /*if((gamepad1.left_stick_x==0)&&(gamepad1.left_stick_y==0))/if either triggers are pressed/{
-
-                if (gamepad1.left_trigger != 0)/if the left trigger is pressed/{
-                    //CCW zero clarence rotation (speed determined my magnitude of left trigger)
-                    motorA.setPower(-(gamepad1.right_trigger));
-                    motorB.setPower(gamepad1.right_trigger);
-                    motorC.setPower(gamepad1.right_trigger);
-                    motorD.setPower(-(gamepad1.right_trigger));
-                }else/if only the right trigger is pressed/{
-                    //CW zero clarence rotation (speed determined my magnitude of right trigger)
-                    motorA.setPower(gamepad1.left_trigger);
-                    motorB.setPower(-(gamepad1.left_trigger));
-                    motorC.setPower(-(gamepad1.left_trigger));
-                    motorD.setPower(gamepad1.left_trigger);
-                }
-
-            }else/if neither triggers are pressed/{
-
-                //translational movement determined by vector of left joystick
-                motorA.setPower((Math.sin(joystickTheta-(Math.PI/4)))*joystickRadius); //same power as motor C but in opposite direction
-                motorB.setPower((Math.cos(joystickTheta-(Math.PI/4)))*joystickRadius); //same power as motor D but in opposite direction
-                motorC.setPower((Math.sin(joystickTheta-(Math.PI/4)))*joystickRadius);
-                motorD.setPower((Math.cos(joystickTheta-(Math.PI/4)))*joystickRadius);
-
-            }*/
-
+            
             double joystickTheta = Math.atan2((gamepad1.left_stick_y),(gamepad1.left_stick_x)); //declares the angle of joystick position in standard polar coordinates
             double joystickRadius = Math.sqrt((gamepad1.left_stick_x)*(gamepad1.left_stick_x)+(gamepad1.left_stick_y)*(gamepad1.left_stick_y)); //declares the magnitude of the radius of the joystick position
 
