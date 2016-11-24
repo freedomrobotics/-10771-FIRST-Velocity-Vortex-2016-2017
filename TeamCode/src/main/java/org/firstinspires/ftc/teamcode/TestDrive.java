6ab61e91663b7abcbfdf203fe8ae2add2481efd4
BuildingAtom <84liadam@gmail.com>
2016-11-23 21:37:12 -0500

    - The front right (motorA) and the back left (motorC) motors were reversed - Halved rotationPower value - motors A and D rotationalPower values inverted - motors C and D have signage on shaft power changed to positive
    Reasons and explanations are found in the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
@@ -26,6 +26,19 @@ public class TestDrive extends LinearOpMode{
         motorC = hardwareMap.dcMotor.get("motorBackLeft"); //sets variable motorC to back left motor
         motorD = hardwareMap.dcMotor.get("motorBackRight"); //sets variable motorD to back right motor
 
+        /*
+        In our mecanum setup, the two front wheels are chain driven and the two rear wheels are
+         direct gear driven. What this means is that the two front wheels are spinning the same
+         direction as the motor and the two rear wheels are spinning the opposite. Since the
+         motors on the right need to be spinning clockwise to move forward, the motors on the
+         right need to be reversed. Thus the front right and the back left motors need to be
+         reversed. That is, motors A and C. - Adam Li
+         */
+        motorA.setDirection(DcMotor.Direction.REVERSE);
+        motorB.setDirection(DcMotor.Direction.FORWARD);
+        motorC.setDirection(DcMotor.Direction.REVERSE);
+        motorD.setDirection(DcMotor.Direction.FORWARD);
+
         waitForStart();
         while(opModeIsActive()){
             
@@ -34,13 +47,20 @@ public class TestDrive extends LinearOpMode{
 
             double ACShaftPower = -((Math.sin(joystickTheta-(Math.PI/4)))*joystickRadius); //sets the power of the shaft containing motors A and C using the radius to scale the sin value of the joystickTheta
             double BDShaftPower = -((Math.cos(joystickTheta-(Math.PI/4)))*joystickRadius); //sets the power of the shaft containing motors B and D using the radius to scale the cos value of the joystickTheta
-            double rotationalPower = (gamepad1.right_trigger)-(gamepad1.left_trigger); //sets the power of rotation by finding the difference between the left and right triggers
+            // Halved rotationPower value to allow for simultaneous translation and rotation when fully depressed - Adam Li
+            double rotationalPower = (gamepad1.right_trigger - gamepad1.left_trigger) / 2; //sets the power of rotation by finding the difference between the left and right triggers
 
             //sets the motor power where the ratio of input from translational motion is dictated by the magnitude of the rotational motion
-            motorA.setPower((rotationalPower) + ((ACShaftPower)*(1.0 - (Math.abs(rotationalPower)))));
+            /*
+            We know the right side needs to be driven in reverse to rotate right, so motors A
+             and D have negative rotationalPower values. We also know forward translation
+             involves positive motor values for all when rotation is disregarded, so motors C
+             and D have signage on shaft power changed to positive again.
+             */
+            motorA.setPower((-rotationalPower) + ((ACShaftPower)*(1.0 - (Math.abs(rotationalPower)))));
             motorB.setPower((rotationalPower) + ((BDShaftPower)*(1.0 - (Math.abs(rotationalPower)))));
-            motorC.setPower((rotationalPower) - ((ACShaftPower)*(1.0 - (Math.abs(rotationalPower)))));
-            motorD.setPower((rotationalPower) - ((BDShaftPower)*(1.0 - (Math.abs(rotationalPower)))));
+            motorC.setPower((rotationalPower) + ((ACShaftPower)*(1.0 - (Math.abs(rotationalPower)))));
+            motorD.setPower((-rotationalPower) + ((BDShaftPower)*(1.0 - (Math.abs(rotationalPower)))));
 
             idle();
 
