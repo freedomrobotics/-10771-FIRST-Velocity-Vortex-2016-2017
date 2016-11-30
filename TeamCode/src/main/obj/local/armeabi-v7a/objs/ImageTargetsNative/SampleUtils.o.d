dd925e13b1e19e65814270022b2a8fc8801f920e
DESKTOP-OHJ0N56\Shabi <shabirhossain130@gmail.com>
2016-11-30 16:15:44 -0500

    Revert "Setup native code and imported an example (that is nonworking because it is imported). It may or may not be 100% working, but it should be enough to start with."
    This reverts commit 794e8c190bb0b48c6fa0980fbe98b2bfda8b3f8f.

--- TeamCode/src/main/obj/local/armeabi-v7a/objs/ImageTargetsNative/SampleUtils.o.d
+++
@@ -1,5 +0,0 @@
-D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/obj/local/armeabi-v7a/objs/ImageTargetsNative/SampleUtils.o: \
-  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/SampleUtils.cpp \
-  D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/SampleUtils.h
-
-D:/FTC/2016-2017/-10771-FIRST-Velocity-Vortex-2016-2017/TeamCode/src/main/jni/SampleUtils.h:
