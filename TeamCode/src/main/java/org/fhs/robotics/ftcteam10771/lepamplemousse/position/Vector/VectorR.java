103db899513ff9261d3c9de67bfb3eca1e14bcf6
BuildingAtom <84liadam@gmail.com>
2016-12-11 13:42:26 -0500

    functions for VectorR

--- TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/position/Vector/VectorR.java
+++ TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/position/Vector/VectorR.java
@@ -11,4 +11,35 @@ public class VectorR {
     Coordinate coordinate;
     Rotation rotation;
 
+    public float getX(){
+        return 0;
+    }
+    public float getY(){
+        return 0;
+    }
+    public void setX(float x){}
+    public void setY(float y){}
+    public float getRad(){
+        return 0;
+    }
+    public void setRad(float rad){}
+    public float getDeg(){
+        return 0;
+    }
+    public void setDeg(float deg){}
+    public float getRawR(){
+        return 0;
+    }
+    public void setRawR(float r){}
+    public void setAllRaw(float x, float y, float r){}
+    public void setAllRad(float x, float y, float rad){}
+    public void setAllDeg(float x, float y, float deg){}
+    public void setTheta(float theta){}
+    public void setRadius(float radius){}
+    public float getTheta(){
+        return 0;
+    }
+    public float getRadius(){
+        return 0;
+    }
 }
