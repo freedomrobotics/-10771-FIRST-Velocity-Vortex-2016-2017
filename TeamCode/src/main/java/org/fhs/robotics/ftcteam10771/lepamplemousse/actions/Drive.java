fd1a40b71f76d0b3098de8f47d8965e41d51c6b3
Joel Veneracion <84veneracionj@gmail.com>
2016-12-12 20:50:42 -0500

    Framework implementation- Adam and Matt

--- TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/actions/Drive.java
+++ TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/actions/Drive.java
@@ -1,8 +1,54 @@
 package org.fhs.robotics.ftcteam10771.lepamplemousse.actions;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.fhs.robotics.ftcteam10771.lepamplemousse.config.Config.ParsedData;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.position.entities.Robot;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.position.vector.VectorR;
+
 /**
  * Created by Adam Li on 10/27/2016.
  * Class to manage how the robot drives (smart control schemes, drivetrain types, etc.)
  */
 public class Drive {
+    VectorR vectorR;
+    Robot robot;
+    DcMotor frMotor;
+    DcMotor flMotor;
+    DcMotor brMotor;
+    DcMotor blMotor;
+    ParsedData settings;
+
+    public enum MOTORS {
+        FRMOTOR;
+    }
+
+    public Drive(VectorR vectorR, Robot robot, DcMotor frMotor,
+                 DcMotor flMotor, DcMotor brMotor, DcMotor blMotor,
+                 ParsedData settings){
+        this.vectorR = vectorR;
+        this.robot = robot;
+        this.frMotor = frMotor;
+        this.flMotor = flMotor;
+        this.brMotor = brMotor;
+        this.blMotor = blMotor;
+
+        settings.getFloat("wheel");
+        settings.subData("lr_motor").getBool("reversed");
+    }
+
+    public void init(){
+
+    }
+
+    public void begin(){
+
+    }
+
+    public void setDirection(MOTORS motor, DcMotorSimple.Direction direction){
+        if(motor == MOTORS.FRMOTOR){
+            frMotor.setDirection(direction);
+        }
+    }
 }
