9877ed977041c351888b0b37f1fe218d11fade04
Matthew <matthew.r.sabatino@gmail.com>
2016-12-15 20:56:41 -0500

    drive code relative to robot put in seperate driveThread

---
+++ TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/actions/VectorDrive.java
@@ -0,0 +1,68 @@
+package org.fhs.robotics.ftcteam10771.lepamplemousse.actions;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.config.Config;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.position.entities.Robot;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.position.vector.VectorR;
+import java.lang.Math;
+
+/**
+ * Created by Adam Li on 10/27/2016.
+ * Class to manage how the robot drives (smart control schemes, drivetrain types, etc.)
+ */
+public class VectorDrive {
+    VectorR vectorR;
+    Robot robot;
+    DcMotor frMotor;
+    DcMotor flMotor;
+    DcMotor brMotor;
+    DcMotor blMotor;
+    Config.ParsedData settings;
+    boolean vectorDriveActive;
+
+    Runnable driveRunnable = new Runnable() {
+        @Override
+        public void run() {
+
+            while (!Thread.interrupted()) {
+                double joystickTheta = vectorR.getTheta();
+                double joystickRadius = vectorR.getRadius();
+                double rotationalPower = vectorR.getRadius();
+
+                double ACShaftPower = ((Math.sin(joystickTheta - (Math.PI / 4))) * joystickRadius);
+                double BDShaftPower = ((Math.cos(joystickTheta - (Math.PI / 4))) * joystickRadius);
+
+                frMotor.setPower((-joystickRadius) + ((ACShaftPower) * (1.0 - (Math.abs(rotationalPower)))));
+                flMotor.setPower((joystickRadius) + ((BDShaftPower) * (1.0 - (Math.abs(rotationalPower)))));
+                blMotor.setPower((joystickRadius) + ((ACShaftPower) * (1.0 - (Math.abs(rotationalPower)))));
+                brMotor.setPower((-joystickRadius) + ((BDShaftPower) * (1.0 - (Math.abs(rotationalPower)))));
+            }
+
+        }
+    };
+    Thread driveThread = new Thread(driveRunnable);
+
+    public VectorDrive(VectorR vectorR, Robot robot, DcMotor frMotor,
+                       DcMotor flMotor, DcMotor brMotor, DcMotor blMotor,
+                       Config.ParsedData settings){
+
+        this.vectorR = vectorR;
+        this.robot = robot;
+        this.frMotor = frMotor;
+        this.flMotor = flMotor;
+        this.brMotor = brMotor;
+        this.blMotor = blMotor;
+        this.settings = settings;
+        this.vectorDriveActive = false;
+
+    }
+
+    public void startDriveThread(){
+        driveThread.start();
+        vectorDriveActive = true;
+    }
+
+    public void endDriveThread(){
+        driveThread.interrupt();
+        vectorDriveActive = false;
+    }
+}
