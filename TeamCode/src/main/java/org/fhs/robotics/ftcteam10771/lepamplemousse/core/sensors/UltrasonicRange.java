dc17101c8f2faaf6bf10263443d2f234ffc1cec9
Joel Veneracion <84veneracionj@gmail.com>
2017-01-11 22:05:06 -0500

    Added RGB handler class and Ultrasonic range sensor handling class

---
+++ TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/core/sensors/UltrasonicRange.java
@@ -0,0 +1,68 @@
+package org.fhs.robotics.ftcteam10771.lepamplemousse.core.sensors;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
+
+/**
+ * Class that handles the Adafruit
+ * Range sensor
+ *
+ * Created by joelv on 1/11/2017.
+ */
+public class UltrasonicRange {
+
+    private AnalogInput rangeSensor;
+    private DigitalChannel toggler;
+    private final double scaleFactor = 5.0 / 512.0;
+
+    /*
+        Default constructors
+     */
+    UltrasonicRange() {
+
+    }
+
+    /**
+     * Constructor for a full range sensor
+     *
+     * @param analogInput    the device which gives voltage inputs
+     * @param digitalChannel the output that gives boolean state
+     */
+    UltrasonicRange(AnalogInput analogInput, DigitalChannel digitalChannel) {
+        rangeSensor = analogInput;
+        toggler = digitalChannel;
+        //todo test to see if it works
+        toggler.setMode(DigitalChannelController.Mode.OUTPUT);
+    }
+
+    /**
+     * Enable or disable the range sensor
+     * @param state true for on or false for off
+     */
+    public void enable(boolean state) {
+        toggler.setState(state);
+    }
+
+    /**
+     * Gives the distance in inches
+     *
+     * @return the distance in inches
+     */
+    public double distance() {
+        if (rangeSensor != null && toggler != null) {
+            if (toggler.getState()) {
+                return rangeSensor.getVoltage() / scaleFactor;
+            }
+            return 404;
+        }
+        return 0;
+    }
+
+    /**
+     * Getter for range sensor state
+     */
+    public boolean ultrasonicState(){
+        return toggler.getState();
+    }
+}
