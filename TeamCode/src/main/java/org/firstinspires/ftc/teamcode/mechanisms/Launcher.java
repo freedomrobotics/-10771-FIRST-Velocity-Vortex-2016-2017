ba6a00d8f99b4d669ab86d84f9c0e836165f066e
Joel Veneracion <84veneracionj@gmail.com>
2017-01-13 00:37:47 -0500

    Created a blank class to work the ball launcher code on

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Launcher.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+/**
+ * Class that handles the ball launcher mechanism motors and servos
+ * Created by joelv on 1/13/2017.
+ */
+public class Launcher {
+}
