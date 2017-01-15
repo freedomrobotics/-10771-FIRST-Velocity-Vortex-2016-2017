cd32c11eb82cdc7df3de1756b027adfb7a50e4a0
BuildingAtom <84liadam@gmail.com>
2017-01-15 00:07:51 -0500

    adapted testdrive for framework to test framework

--- TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/modes/TestDrive3.java
+++ TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/modes/TestDrive3.java
@@ -1,14 +1,21 @@
 package org.fhs.robotics.ftcteam10771.lepamplemousse.modes;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
-import java.util.HashMap;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.config.Config;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.core.Components;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.core.Controllers;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.core.components.Aliases;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.core.vars.Static;
+
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Matthew on 11/14/2016.
@@ -18,29 +25,88 @@ import java.util.Map;
 public class TestDrive3 extends LinearOpMode{
 
     //initializes motors in order of standard graph quadrants
-    private DcMotor motorA;
-    private DcMotor motorB;
-    private DcMotor motorC;
-    private DcMotor motorD;
+    private DcMotor motorFR;
+    private DcMotor motorFL;
+    private DcMotor motorBL;
+    private DcMotor motorBR;
     private DcMotor intakeMotor;
     private double intakePower = 0.0;
     private boolean intakeF = false, intakeB = false;
     private List<String> toggle = new LinkedList<>();
+    private Config rawSettings;
+    private Config.ParsedData settings;
+    private Components components;
+    private Controllers controls;
+
+    private static final String TAG = "TestDrive3Debug";
 
     @Override
     public void runOpMode() throws InterruptedException{
 
-        motorA = hardwareMap.dcMotor.get("motorFrontRight"); //sets variable motorA to front right motor
-        motorB = hardwareMap.dcMotor.get("motorFrontLeft"); //sets variable motorB to front left motor
-        motorC = hardwareMap.dcMotor.get("motorBackLeft"); //sets variable motorC to back left motor
-        motorD = hardwareMap.dcMotor.get("motorBackRight"); //sets variable motorD to back right motor
+        Config keymapping = new Config(Static.configPath, Static.configControlFileName + Static.configFileSufffix, telemetry, "keymapping");
+
+        Log.d(TAG, "keymapping");
+
+        // should catch other errors, but oh well
+        // add data logging
+        if (keymapping.read() == Config.State.DEFAULT_EXISTS) {
+            keymapping.create(true);
+            Log.d(TAG, "keymapping-read-fail-create");
+            //read without creating file if it still fails.
+            if (keymapping.read() == Config.State.DEFAULT_EXISTS)
+                keymapping.read(true);
+                Log.d(TAG, "keymapping-read-again");
+        }
+
+        Config components = new Config(Static.configPath, Static.configCompFileName + Static.configFileSufffix, telemetry, "components");
+        Log.d(TAG, "components");
+        if (components.read() == Config.State.DEFAULT_EXISTS) {
+            components.create(true);
+            Log.d(TAG, "components-read-fail-create");
+            if (components.read() == Config.State.DEFAULT_EXISTS)
+                components.read(true);
+                Log.d(TAG, "components-read-again");
+        }
+
+        rawSettings = new Config(Static.configPath, Static.configVarFileName + Static.configFileSufffix, telemetry, "settings");
+        Log.d(TAG, "settings");
+        if (rawSettings.read() == Config.State.DEFAULT_EXISTS) {
+            rawSettings.create(true);
+            Log.d(TAG, "settings-read-fail-create");
+            if (rawSettings.read() == Config.State.DEFAULT_EXISTS)
+                rawSettings.read(true);
+                Log.d(TAG, "settings-read-again");
+        }
+
+        settings = rawSettings.getParsedData();
+        Log.d(TAG, "settings-parse");
+
+
+        this.components = new Components(hardwareMap, telemetry, components);
+        Log.d(TAG, "components-object");
+        this.components.initialize();
+        Log.d(TAG, "components-init");
+        controls = new Controllers(gamepad1, gamepad2, keymapping);
+        Log.d(TAG, "controllers-init");
+
+        //sets variables to motors
+        motorFR = Aliases.motorMap.get(settings.subData("drivetrain").subData("motor").subData("front_right").getString("map_name"));
+        motorFL = Aliases.motorMap.get(settings.subData("drivetrain").subData("motor").subData("front_left").getString("map_name"));
+        motorBL = Aliases.motorMap.get(settings.subData("drivetrain").subData("motor").subData("back_left").getString("map_name"));
+        motorBR = Aliases.motorMap.get(settings.subData("drivetrain").subData("motor").subData("back_right").getString("map_name"));
+
+        Log.d(TAG, "motor setup");
 
         intakeMotor = hardwareMap.dcMotor.get("motorIntake");
 
-        motorA.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorC.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorD.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Log.d(TAG, "motor setup 2");
+
+        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        Log.d(TAG, "motor setup 3");
 
         /*
         In our mecanum setup, the two front wheels are chain driven and the two rear wheels are
@@ -50,10 +116,21 @@ public class TestDrive3 extends LinearOpMode{
          right need to be reversed. Thus the front right and the back left motors need to be
          reversed. That is, motors A and C. - Adam Li
          */
-        motorA.setDirection(DcMotor.Direction.REVERSE);
-        motorB.setDirection(DcMotor.Direction.FORWARD);
-        motorC.setDirection(DcMotor.Direction.REVERSE);
-        motorD.setDirection(DcMotor.Direction.FORWARD);
+        /*motorFR.setDirection(DcMotor.Direction.REVERSE);
+        motorFL.setDirection(DcMotor.Direction.FORWARD);
+        motorBL.setDirection(DcMotor.Direction.REVERSE);
+        motorBR.setDirection(DcMotor.Direction.FORWARD);*/
+
+        motorFR.setDirection(DcMotor.Direction.FORWARD);
+        motorFL.setDirection(DcMotor.Direction.FORWARD);
+        motorBL.setDirection(DcMotor.Direction.FORWARD);
+        motorBR.setDirection(DcMotor.Direction.FORWARD);
+        if (settings.subData("drivetrain").subData("motor").subData("front_right").getBool("reversed")) motorFR.setDirection(DcMotor.Direction.REVERSE);
+        if (settings.subData("drivetrain").subData("motor").subData("front_left").getBool("reversed")) motorFL.setDirection(DcMotor.Direction.REVERSE);
+        if (settings.subData("drivetrain").subData("motor").subData("back_left").getBool("reversed")) motorBL.setDirection(DcMotor.Direction.REVERSE);
+        if (settings.subData("drivetrain").subData("motor").subData("back_right").getBool("reversed")) motorBR.setDirection(DcMotor.Direction.REVERSE);
+
+        Log.d(TAG, "motor setup 4");
 
         waitForStart();
         while(opModeIsActive()){
@@ -88,10 +165,10 @@ public class TestDrive3 extends LinearOpMode{
             double powerB = (BDRotationalPower)+(BDShaftPower*(1.0-Math.abs(BDRotationalPower)));
             double powerC = (ACRotationalPower)+(ACShaftPower*(1.0-Math.abs(ACRotationalPower)));
             double powerD = (-BDRotationalPower)+(BDShaftPower*(1.0-Math.abs(BDRotationalPower)));
-            motorA.setPower(Range.scale(powerA, -1, 1, -.778, .778));
-            motorB.setPower(Range.scale(powerB, -1, 1, -.778, .778));
-            motorC.setPower(Range.scale(powerC, -1, 1, -.778, .778));
-            motorD.setPower(Range.scale(powerD, -1, 1, -.778, .778));
+            motorFR.setPower(Range.scale(powerA, -1, 1, -.778, .778));
+            motorFL.setPower(Range.scale(powerB, -1, 1, -.778, .778));
+            motorBL.setPower(Range.scale(powerC, -1, 1, -.778, .778));
+            motorBR.setPower(Range.scale(powerD, -1, 1, -.778, .778));
 
             telemetry.addData("Speed-FR", powerA);
             telemetry.addData("Speed-FL", powerB);
@@ -128,5 +205,6 @@ public class TestDrive3 extends LinearOpMode{
             //idle();
 
         }
+        Aliases.clearAll();
     }
 }
