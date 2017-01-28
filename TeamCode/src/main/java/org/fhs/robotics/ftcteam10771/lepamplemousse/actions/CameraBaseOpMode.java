4bba4d53f93b91dc300a8495d92a30855b8b5469
Joel Veneracion <84veneracionj@gmail.com>
2017-01-28 12:15:10 -0500

    commit

--- TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/actions/CameraBaseOpMode.java
+++ TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/actions/CameraBaseOpMode.java
@@ -17,24 +17,9 @@ public class CameraBaseOpMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         cameraVision = new CameraVision();
-        //range = new UltrasonicRange2(hardwareMap.analogInput.get("range"), hardwareMap.digitalChannel.get("switch"));
-        Thread opThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (!Thread.interrupted()){
-                    cameraVision.runImageTracking();
-                    //range.streamDistance();
-                }
-            }
-        });
         cameraVision.setUnitToRadians(false);
         waitForStart();
-
-        //demo of feature I added that does what I think you hoped for the stream to do.
-        //range.toggleStream(true);
-        //range.setAveragingTime(100);
-
-        opThread.start();
+        cameraVision.cameraThread.start();
         double totalBase = 0.0;
         double totalRatio = 0.0;
         int iterations = 0;
@@ -44,20 +29,28 @@ public class CameraBaseOpMode extends LinearOpMode {
                 iterations++;
             }
             else cameraVision.setTargetImage(null);
-            telemetry.addData("Image", cameraVision.getTargetedImage().getName());
-            telemetry.addData("Camera Vision Distance", cameraVision.getZ(cameraVision.getTargetedImage()));
-            if (cameraVision.imageInSight(cameraVision.getTargetedImage()) && cameraVision.getZ(cameraVision.getTargetedImage())!=0f){
+            if (cameraVision.imageInSight(cameraVision.getTargetedImage())){
                 totalBase  += getBase();
                 telemetry.addData("Base", getBase());
                 telemetry.addData("Average Base", totalBase / iterations);
+                telemetry.addData("Image", cameraVision.getTargetedImage().getName());
+                telemetry.addData("Camera Vision Distance", cameraVision.getZ(cameraVision.getTargetedImage()));
+            
             }
-            telemetry.addData("Angle To Turn", cameraVision.getAngleToTurn(cameraVision.getTargetedImage()));
+            else{
+                totalBase  += getBase();
+                telemetry.addData("Base", "null");
+                telemetry.addData("Average Base", "null");
+                telemetry.addData("Image", "null");
+                telemetry.addData("Camera Vision Distance", "null");
+            }
             telemetry.update();
         }
-        opThread.interrupt();
+        cameraVision.cameraThread.interrupt();
     }
 
     public double getBase(){
-        return Math.pow(cameraVision.getX(cameraVision.getTargetedImage()), 1.0 / (cameraVision.getZ(cameraVision.getTargetedImage())));
+        return cameraVision.getZ(cameraVision.getTargetedImage())!=0f ? Math.pow(cameraVision.getX(cameraVision.getTargetedImage()), 1.0 / (cameraVision.getZ(cameraVision.getTargetedImage()))) :
+                0.0;
     }
 }
