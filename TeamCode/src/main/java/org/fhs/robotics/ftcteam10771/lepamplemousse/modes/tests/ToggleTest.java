b28761cc3f191aa9fdf2f8534561e571a13c635d
BuildingAtom <84liadam@gmail.com>
2017-02-17 02:37:33 -0500

    ToggleTester

---
+++ TeamCode/src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/modes/tests/ToggleTest.java
@@ -0,0 +1,51 @@
+package org.fhs.robotics.ftcteam10771.lepamplemousse.modes.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.fhs.robotics.ftcteam10771.lepamplemousse.config.Config;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.core.Controllers;
+import org.fhs.robotics.ftcteam10771.lepamplemousse.core.vars.Static;
+
+/**
+ * Created by Adam Li on 2/17/2017.
+ */
+@TeleOp(name = "ToggleTest")
+public class ToggleTest extends OpMode {
+    private Controllers controls;
+
+    /**
+     * User defined init method
+     * <p>
+     * This method will be called once when the INIT button is pressed.
+     */
+    @Override
+    public void init() {
+        Config keymapping = new Config(Static.configPath, Static.configControlFileName + Static.configFileSufffix, telemetry, "keymapping");
+
+        // should catch other errors, but oh well
+        // add data logging
+        if (keymapping.read() == Config.State.DEFAULT_EXISTS) {
+            keymapping.create(true);
+            //read without creating file if it still fails.
+            if (keymapping.read() == Config.State.DEFAULT_EXISTS)
+                keymapping.read(true);
+        }
+
+        controls = new Controllers(gamepad1, gamepad2, keymapping);
+        controls.initialize();
+    }
+
+    /**
+     * User defined loop method
+     * <p>
+     * This method will be called repeatedly in a loop while this op mode is running
+     */
+    @Override
+    public void loop() {
+        telemetry.addData("Toggle", controls.getToggle("launch"));
+        telemetry.addData("Digital", controls.getDigital("launch"));
+        telemetry.update();
+    }
+}
